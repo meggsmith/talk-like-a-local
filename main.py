2636e06df1e4e7e9a5743ebe266f93df8e22d6ed
meggsmith <meggie@google.com>
2016-06-10 12:18:26 -0700

    added comments to main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import webapp2 #lines 17-19 are imported libraries 
 import jinja2
 import os
 
@@ -29,31 +29,31 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write('anyone home')
 
 
-class SlangHandler(webapp2.RequestHandler):
-    def get(self):
-        slang_template=my_env.get_template("templates/location.html")
-        self.response.write(slang_template.render())
+class SlangHandler(webapp2.RequestHandler): #newly created handler to process slang_finder method and rendering our output.html page
+    def get(self): #the get request
+        slang_template=my_env.get_template("templates/location.html") #the route to the form template, using Jinja2 magic
+        self.response.write(slang_template.render()) #the request to render the form template that uses the Jinga2 magic
 
-    def post(self):
-      def slang_finder(location):
+    def post(self): #the post request
+      def slang_finder(location): #the slang_finder method that will take the action on our user input to take in the location parameter's arguemnt and output the slang list for that location
           slang_dictionary = {'Boston':['wicked', 'pissah', 'bubbler', 'chowdah', 'lobstah'],
-          'New York':['fuh-gedd-about-it', 'hipster', 'bodega', 'bridge and tunnel', 'schmear']}  
+          'New York':['fuh-gedd-about-it', 'hipster', 'bodega', 'bridge and tunnel', 'schmear']}  #lines 39 - 40 are the hard coded dictionaries of relevant slang for possible location arguments
           if location == 'Boston':
             slang_list_for_location = slang_dictionary['Boston']
           elif location == 'New York':
             slang_list_for_location = slang_dictionary['New York']
-          return(slang_list_for_location)
+          return(slang_list_for_location) #lines 41 -45 are the conditional statements that determine the location arguments (inputs) and decide which slang list dictionary to select to show the user
 
-      requested_location=self.request.get('location')
-      slang_list=slang_finder(requested_location)
+      requested_location=self.request.get('location') #newly created variable to extract the location parameter from the input form....aka the get request
+      slang_list=slang_finder(requested_location) #newly created variable to use the method on the users chosen location
 
-      my_slang_dict={'user_slang':slang_list, 'output_city':requested_location}
-      slang_template=my_env.get_template('templates/output.html')
-      self.response.write(slang_template.render(my_slang_dict))
+      my_slang_dict={'user_slang':slang_list, 'output_city':requested_location} #newly created dictionary assiging new variables - slang_list and requested_location - that will eventually be shown to the user
+      slang_template=my_env.get_template('templates/output.html') #the newly created variable that contains the output template. This holds the output.html page - the holding cotainer ready to render the write response.
+      self.response.write(slang_template.render(my_slang_dict)) #code to render the output template passing the dictionary of variables to display to the user
   
             
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/slang', SlangHandler)
+    ('/slang', SlangHandler) #root url to render/display the page using the localhost
 ], debug=True)
